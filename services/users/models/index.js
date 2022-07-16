f5dec5ebbe83745329bedee3b9de725772f67b56
Dita Larasati <ditaisy@gmail.com>
2022-07-17 00:15:51 +0700

    prevent return password

--- services/users/models/index.js
+++ services/users/models/index.js
@@ -2,12 +2,14 @@ const { ObjectId } = require('mongodb');
 const { getMongoDb } = require('../config/mongo');
 
 class UserModel {
+  static projection = { password: 0 }
+
   static async count(payload) {
     return await getMongoDb().collection('users').countDocuments({ ...payload });
   }
   
   static async create(payload) {
-    return await getMongoDb().collection('users').insertOne({ ...payload });
+    return await getMongoDb().collection('users').insertOne({ ...payload }, { projection: this.projection })
   }
   
   static async findOne(payload) {
@@ -15,18 +17,22 @@ class UserModel {
   }
 
   static async findById(id) {
-    return await getMongoDb().collection('users').findOne({ _id: ObjectId(id) });
+    return await getMongoDb().collection('users')
+      .findOne({ _id: ObjectId(id) }, { projection: this.projection })
   }
 
   static async find() {
-    return await getMongoDb().collection('users').find().toArray();
+    return await getMongoDb().collection('users')
+      .find({}, { projection: this.projection }).toArray()
   }
 
   static async findByIdAndUpdate(id, payload) {
-    return await getMongoDb().collection('users').findOneAndUpdate({ _id: ObjectId(id) }, { $set: payload });
+    return await getMongoDb().collection('users')
+      .findOneAndUpdate({ _id: ObjectId(id) }, { $set: payload }, { projection: this.projection })
   }
   static async findByIdAndDelete(id) {
-    return await getMongoDb().collection('users').findOneAndDelete({ _id: ObjectId(id) });
+    return await getMongoDb().collection('users')
+      .findOneAndDelete({ _id: ObjectId(id) }, { projection: this.projection })
   }
 }
 
